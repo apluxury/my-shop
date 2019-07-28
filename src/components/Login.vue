4d7b00602f7e1001878805cfcc997d97eed91528
paluxury <1091758704@qq.com>
2019-07-28 15:14:07 +0800

    login功能

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: pink;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 3px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//element 配置
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//引入公共样式
+import './assets/css/global.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
+
+
+
+
+
+
